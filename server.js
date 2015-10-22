02d76457c6d6594f28307bfffa919b0a6a212d0c
Auster Chen <auster.s.chen@gmail.com>
2015-10-21 19:57:30 -0700

    added compression

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require("babel/register");
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require("compression");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -15,7 +15,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//middleware set up
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
