4a05121dabb716f776ff12ec75a956734afcf6f5
Auster Chen <auster.s.chen@gmail.com>
2015-10-21 19:48:59 -0700

    added babel module

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("babel/register");
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -14,7 +15,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
