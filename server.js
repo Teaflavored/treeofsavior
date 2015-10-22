68d0817903f61b39822ab92027762af07695d39d
Auster Chen <auster.s.chen@gmail.com>
2015-10-21 20:08:49 -0700

    setting up logging to logs/app.log

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require("compression");
+var fs = require("fs");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -18,7 +19,12 @@ app.set('view engine', 'hbs');
 //middleware set up
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(compression());
-app.use(logger('dev'));
+
+//logging
+app.use(logger("dev", {
+    stream: fs.createWriteStream( __dirname + "/logs/app.log", { "flags" : "w" })
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
