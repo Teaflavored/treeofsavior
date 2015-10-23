46a75f951b41eb7e360e5aafac26c87e4345cd7f
Auster Chen <auster.s.chen@gmail.com>
2015-10-22 22:52:19 -0700

    added bootstrap sass to part of the build

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,8 +1,17 @@
+var path = require("path");
+
 module.exports = {
-    entry: ["./app/app.js"],
+    context: path.resolve(__dirname, ".."),
+    entry: {
+        main: [
+            "bootstrap-sass!./app/bootstrap.config.js",
+            "./app/app.js"
+        ]
+    },
     output: {
         path: "./public/javascripts/",
-        filename: "[name].js"
+        filename: "[name].js",
+        chunkFilename: "[name].js"
     },
     module: {
         loaders: [
@@ -10,7 +19,38 @@ module.exports = {
                 test: /\.js?$/,
                 exclude: /node_modules/,
                 loader: "babel"
+            },
+            {
+                test: /\.scss$/,
+                loader: "style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap"
+            },
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
             }
         ]
+    },
+    resolve: {
+        moduleDirectories: [
+            "app",
+            "node_modules"
+        ],
+        extensions: ["", ".json", ".js"]
     }
 };
